4f9c3fdb25b4b10cbc289966ee005d5acdff5637
Kirda <kirdasmooth@gmail.com>
2022-03-05 18:04:16 +0100

    Added variables and more callbacks

--- lang/vm.php
+++ lang/vm.php
@@ -3,8 +3,17 @@
 namespace Premavaca;
 
 const CALLBACKS = [
-    0 => 'cmd_write',
-    1 => 'cmd_write_ascii'
+    0b000000000 => 'cmd_write',
+    0b000000001 => 'cmd_write_ascii',
+    0b000000010 => 'cmd_add',
+    0b000000011 => 'cmd_sub',
+    0b000000100 => 'cmd_mul',
+    0b000000101 => 'cmd_div',
+    0b100000001 => 'cmd_label',
+    0b100000000 => 'cmd_goto',
+    0b110000000 => 'cmd_goto_eq',
+    0b111000000 => 'cmd_goto_neq',
+    0b111111111 => 'cmd_mkvar'
 ];
 
 class State
@@ -12,10 +21,14 @@ class State
     public $arguments;
     public $arg_count;
 
-    public function __construct($arguments = null)
+    public $position;
+
+    public function __construct($position, $arguments = null)
     {
         $this->arguments = $arguments !== null ? $arguments : [];
         $this->arg_count = count($this->arguments);
+
+        $this->position = $position;
     }
 }
 
@@ -25,6 +38,8 @@ class VM
     private $position = 0;
 
     private $variables;
+    private $labels;
+
     private $output;
 
     public function __construct($bytecode)
@@ -32,6 +47,8 @@ class VM
         $this->bytecode = &$bytecode;
 
         $this->variables = [];
+        $this->labels = [];
+
         $this->output = [];
     }
 
@@ -47,7 +64,7 @@ class VM
     {
         switch ($command->op) {
         case OP_CMD:
-            $state = new State($this->pop_arguments());
+            $state = new State($this->position, $this->pop_arguments());
             $id = $this->get_callback_id($command);
 
             if ($id === null)
@@ -98,9 +115,29 @@ class VM
         return $this->bytecode[$this->position++];
     }
 
+    private function select_argument(&$state, $index)
+    {
+        if ($state->arg_count <= $index)
+            throw new \Exception("missing argument #" . ($index + 1));
+
+        return $state->arguments[$index];
+    }
+
+    // If value has been prefixed by `*` then its value will be selected
+    // from `$this->variables` array
     private function get_value($value)
     {
-        return $value->is_variable ? $value->data : $value->data;
+        if ($value->is_variable)
+            return $this->get_variable($value->data);
+
+        return $value->data;
+    }
+
+    private function get_variable($name)
+    {
+        return array_key_exists($name, $this->variables)
+            ? $this->variables[$name]
+            : 0;
     }
 
     private function cmd_write(&$state)
@@ -115,6 +152,83 @@ class VM
         foreach ($state->arguments as $value) {
             echo chr($this->get_value($value));
         }
+    }
+
+    private function cmd_label(&$state)
+    {
+        $name = $this->get_value($this->select_argument($state, 0));
+        $this->labels[$name] = $state->position;
+    }
+
+    private function cmd_goto(&$state)
+    {
+        $name = $this->get_value($this->select_argument($state, 0));
+
+        if (!array_key_exists($name, $this->labels))
+            throw new \Exception("undefined label `$name`");
+
+        $this->position = $this->labels[$name] - 1;
+    }
+
+    private function cmd_goto_eq(&$state)
+    {
+        $left_value = $this->get_value($this->select_argument($state, 1));
+        $right_value = $this->get_value($this->select_argument($state, 2));
+
+        if ($left_value === $right_value)
+            $this->cmd_goto($state);
+    }
+
+    private function cmd_goto_neq(&$state)
+    {
+        $left_value = $this->get_value($this->select_argument($state, 1));
+        $right_value = $this->get_value($this->select_argument($state, 2));
+
+        if ($left_value !== $right_value)
+            $this->cmd_goto($state);
+    }
+
+    private function cmd_add(&$state)
+    {
+        $name = $this->get_value($this->select_argument($state, 0));
+        $increment = $this->get_value($this->select_argument($state, 1));
+
+        $this->variables[$name] = $this->get_variable($name) + $increment;
+    }
+
+    private function cmd_mul(&$state)
+    {
+        $name = $this->get_value($this->select_argument($state, 0));
+        $decrement = $this->get_value($this->select_argument($state, 1));
+
+        $this->variables[$name] = $this->get_variable($name) * $decrement;
+    }
+
+    private function cmd_div(&$state)
+    {
+        $name = $this->get_value($this->select_argument($state, 0));
+        $increment = $this->get_value($this->select_argument($state, 1));
+
+        if ($increment === 0)
+            throw new \Exception("division by 0");
+
+        $this->variables[$name] = $this->get_variable($name) / $increment;
+    }
+
+    private function cmd_sub(&$state)
+    {
+        $name = $this->get_value($this->select_argument($state, 0));
+        $decrement = $this->get_value($this->select_argument($state, 1));
+
+        $this->variables[$name] = $this->get_variable($name) - $decrement;
+    }
+
+    private function cmd_mkvar(&$state)
+    {
+        $name = $this->get_value($this->select_argument($state, 0));
+        $value = $this->get_value($this->select_argument($state, 1));
+
+        $this->variables[$name] = $value;
     }
 }
 
